0811d931026ff0af7ef8ce0a38824ae9aacb2506
Davy Monte <davymonte80@gmail.com>
2026-07-24 15:54:00 +0300

    add host

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,7 +20,10 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS",
+    default=["localhost", "127.0.0.1", "buymeespresso-api.onrender.com"],
+)
 
 INSTALLED_APPS = [
     "django.contrib.admin",
